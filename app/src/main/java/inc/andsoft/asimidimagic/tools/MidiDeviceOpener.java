0116230c57327285c07b73ee21340937b3b89112
Andrea Odetti <mariofutire@gmail.com>
2019-01-14 21:11:03 +0000

    Removed unused import.
    Signed-off-by: Andrea Odetti <mariofutire@gmail.com>

--- app/src/main/java/inc/andsoft/asimidimagic/tools/MidiDeviceOpener.java
+++ app/src/main/java/inc/andsoft/asimidimagic/tools/MidiDeviceOpener.java
@@ -15,7 +15,6 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.Stack;
